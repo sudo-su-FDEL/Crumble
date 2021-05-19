8d3d88397c4990c32689c0e4061a3aea8b4e2637
sudo-su-FDEL <58496330+sudo-su-FDEL@users.noreply.github.com>
2021-05-19 12:40:31 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,16 +3,20 @@
 #include <fstream>
 using namespace std;
 
+//Delete words function
 void delWords(int del_L) {
 
     string StoreWord;
     string filename, newfile;
-
+    //User is prompted for file 
     cout << "Please enter or copy and paste the file" << endl;
     cin >> filename;
+    // New file created
     ifstream WordIFILE(filename);
+    //User prompted for new file name
     cout << "Please enter the name for your new file" << endl;
     cin >> newfile;
+    //file closed
     ofstream OutFile(newfile);
 
     while (getline(WordIFILE, StoreWord)) {
@@ -23,7 +27,7 @@ void delWords(int del_L) {
     }
     WordIFILE.close();
     OutFile.close();
-
+    //store file
     while (getline(WordIFILE, StoreWord)) {
 
         OutFile << StoreWord << endl;
@@ -33,11 +37,12 @@ void delWords(int del_L) {
 }
 
 int main() {
-
+    
     int del_L = 0;
+    //prompt user to enter the amount of letters 
     cout << "Enter the length of the characters to be deleted: ";
     cin >> del_L;
-
+    //function called
     delWords(del_L);
 
     return 0;
